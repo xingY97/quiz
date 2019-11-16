c9bd798c7006531a42d0e2775c52e7abd6a43932
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-11-15 17:16:16 -0800

    second question added

--- quiz.py
+++ quiz.py
@@ -1,28 +1,26 @@
 
-# def question_one():
-#     """asls a true or false question"""
-#     response = int(input("Alaska was the last state to enter the Union, true or false? Please enter 1 for True or 0 for False"))
-#     correct_answer = 1
-#     if response == correct_answer:
-#         print("got it")
-#         return 1
-#     elif response != correct_answer:
-#         print("wrong answer")
-#         return 0
-
-# total_score = 0
-# result = question_one()
-# total_score += result
-# print(total_score)
-
 def multiple_choice():
-    queation_one = print("1.What is the a rough estimate of the population of New York City? Please enter a character a-e")
+    question_one = print("1.What is the a rough estimate of the population of New York City? Please enter a character a-e")
     answer_options = input(" a) 1 million\n b) 3 million\n c) 5 million\n d) 8 million\n e)10 million")
     correct_answer = "d"
     if answer_options.lower() == correct_answer:
         print ("Correct!")
     elif answer_options.lower() != correct_answer:
-        print("Incorrect!")
+        print("Incorrect, the answer is d) 8 million!")
+
+def numeric_response():
+    question_two = int(input("How many states are there in the United States? Please enter an integer?"))
+    correct_answer = 50
+    if question_two == correct_answer:
+        print("Correct!")
+    elif question_two > correct_answer:
+        print ("The number you entered is too large, the correct answer is 50")
+    elif question_two < correct_answer:
+        print ("The number you entered is too small, the correct answer is 50")
+
+def True_False_question():
+    
 
 
 multiple_choice()
+numeric_response()
