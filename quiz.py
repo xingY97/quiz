6d073aa1f113d8aee541ce9f8b09cd32575a3f4e
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-11-18 14:21:39 -0800

    adda list to store correct, and incorrect questions

--- quiz.py
+++ quiz.py
@@ -12,12 +12,12 @@ answer_choices = [" a) 1 million\n b) 3 million\n c) 5 million\n d) 8 million\n 
                 "Enter 1 for True, 0 for false\nyour answer:",
                 "Enter 1 for True, 0 for false\nyour answer:"]
 
-correct_choices = [{"d","8 million"},
-                   {"d","professor Binns"},
+correct_choices = ["d","8 million",
+                   "d","professor Binns",
                    "50",
                    "16",
-                   {"0", "false"},
-                   {"1", "true"}]
+                   "0", "false",
+                   "1", "true"]
 
 answers = ["rough estimate of the population of New York City is 8 million",
            "Professor Binns taught history of Mafic",
@@ -28,16 +28,22 @@ answers = ["rough estimate of the population of New York City is 8 million",
 
 def quiz():
     score = 0
+    check_answers = []
+    
     for question,choices,correct_choice,answer in zip (questions,answer_choices,correct_choices,answers):
         print(question)
         user_answer = input(choices).lower()
         if user_answer in correct_choice:
             print("Correct")
+            index = questions.index(question) +1
+            check_answers.append(f" {index} correct")
             score += 1
         else:
-            print("Incorrect", answer)        
+            print("Incorrect", answer)      
+            index = questions.index(question) +1
+            check_answers.append(f" {index} Incorrect")  
     print("     Correctly answered",score, "out of", len(questions), "that is", round(score / len(questions),2) * 100, "%")
-    
+    print(check_answers)
     if score <= 3:
         print("You didn't do well")
     elif score == 5:
